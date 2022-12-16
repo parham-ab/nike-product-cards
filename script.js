e07951a6401934ac1ab013d93838ed46e5d6a1d8
Parham Abolghasemi <parhamab17@gmail.com>
2022-12-16 22:09:02 +0330

    js script added, related styles to js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-document.querySelector(".shoes-1").addEventListener("click", () => {});
-document.querySelector(".shoes-2").addEventListener("click", () => {});
+const container = document.querySelector(".container");
+const color1 = document.querySelector(".color-1");
+const color2 = document.querySelector(".color-2");
+
+color1.addEventListener("click", () => {
+  container.classList.add("change");
+});
+color2.addEventListener("click", () => {
+  container.classList.remove("change");
+});
