eb9a453750a598690c1f1f777600c3da9dccdbf2
Parham Abolghasemi <parhamab17@gmail.com>
2022-12-17 08:35:21 +0330

    change shoes by color on click

---
+++ script.js
@@ -0,0 +1,55 @@
+// particle.js
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 80, density: { enable: true, value_area: 800 } },
+    color: { value: "#ffffff" },
+    shape: {
+      type: "",
+      stroke: { width: 0, color: "#000000" },
+      polygon: { nb_sides: 5 },
+      image: { src: "img/github.svg", width: 100, height: 100 },
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 2,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: { enable: false, rotateX: 600, rotateY: 1200 },
+    },
+  },
+  retina_detect: true,
+});
+// replace shoe
+const shoe1 = document.querySelector(".shoes-1");
+const shoe2 = document.querySelector(".shoes-2");
+const color1 = document.querySelector(".color-1");
+const color2 = document.querySelector(".color-2");
+
+color2.addEventListener("click", function () {
+  shoe2.style.display = "none";
+  shoe1.style.display = "inline-block";
+});
+color1.addEventListener("click", function () {
+  shoe2.style.display = "inline-block";
+  shoe1.style.display = "none";
+});
